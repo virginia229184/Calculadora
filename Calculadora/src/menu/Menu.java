601e2c0201ac902707cfe52aaf2b1c09ccc4971a
Asier Iza <aiza@fpzornotza.com>
2022-01-14 01:05:16 +0100

    Primera versión calculadora

---
+++ Calculadora/src/menu/Menu.java
@@ -0,0 +1,41 @@
+package menu;
+import java.util.Scanner;
+public class Menu {
+    private static Scanner teclado = new Scanner(System.in);
+    
+    public int[] pedirNumeros(){
+        int[] ret = new int[2];
+        System.out.print ("Operando 1: ");
+        ret [0] = teclado.nextInt();
+        System.out.print ("Operando 2: ");
+        ret [1] = teclado.nextInt();
+        return ret;
+    }
+    
+    public String menuOpciones() {
+        String ret = "";
+        do {
+            System.out.print ("Operaciones [+, -, *, /, %]: ");
+            ret = teclado.next();
+        } while (!((ret.equalsIgnoreCase("+")) || (ret.equalsIgnoreCase("-")) ||
+                (ret.equalsIgnoreCase("*")) || (ret.equalsIgnoreCase("/")) ||
+                (ret.equalsIgnoreCase("%"))
+                ));
+                return ret;
+    }
+    
+    public boolean repetir(){
+        boolean ret = false;
+        String respuesta;
+        do {
+            System.out.print ("¿Desea continuar trabajando con la calculadora? [s / n]");
+            respuesta = teclado.next();
+        } while (!((respuesta.equalsIgnoreCase("s")) || (respuesta.equalsIgnoreCase("n"))
+                    ));
+                    
+        if (respuesta.equalsIgnoreCase("s")){
+            ret = true;
+        }
+        return ret;
+    }
+}
---
+++ Calculadora/src/principal/Calculadora.java
@@ -0,0 +1,38 @@
+package principal;
+import menu.Menu;
+import operaciones.Operaciones;
+
+public class Calculadora{
+    public static void main(String[] args) {   
+        int resultado = 0;
+        String operacion = "";
+        int[] operandos = new int [2];
+        
+        Menu menu = new Menu();
+        Operaciones operaciones = new Operaciones();
+        
+        do{
+            operandos = menu.pedirNumeros();
+            operacion = menu.menuOpciones();
+            
+            if (operacion.equalsIgnoreCase("+")){
+                resultado = operaciones.sumar(operandos[0], operandos[1]);
+                System.out.println ("Resultado: " + resultado);
+            } else if (operacion.equalsIgnoreCase("-")){
+                resultado = operaciones.restar(operandos[0], operandos[1]);
+                System.out.println ("Resultado: " + resultado);
+            } else if (operacion.equalsIgnoreCase("*")){
+                resultado = operaciones.multiplicar(operandos[0], operandos[1]);
+                System.out.println ("Resultado: " + resultado);
+            } else if (operacion.equalsIgnoreCase("/")){
+                resultado = operaciones.dividir(operandos[0], operandos[1]);
+                System.out.println ("Resultado: " + resultado);
+            } else if (operacion.equalsIgnoreCase("%")){
+                resultado = operaciones.resto(operandos[0], operandos[1]);
+                System.out.println ("Resultado: " + resultado);
+            } else {
+                System.out.println ("Operación no válida");
+            }
+        }   while (menu.repetir());
+    }
+}
